d210e48077e2a6ccf49906fe60ba0e9da554a7c5
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-07-25 10:21:35 +0500

    init: start app

--- base/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import FuelPurchase
-
-class FuelPurchaseForm(forms.ModelForm):
-    class Meta:
-        model = FuelPurchase
-        fields = ['customer', 'petrol_type', 'litres']
