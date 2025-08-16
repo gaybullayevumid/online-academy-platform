88669e046b4588d5f69b8a5f4215fee8b5feea81
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-08-16 22:29:23 +0500

    Remove Tailwind CSS and DaisyUI configuration and related files from the theme

--- apps/courses/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CoursesListView
-
-
-app_name = "courses"
-urlpatterns = [
-    path("", CoursesListView.as_view(), name="courses_list"),
-]
--- apps/courses/views.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic import TemplateView
-
-# Create your views here.
-
-class CoursesListView(TemplateView):
-    template_name = "courses/course_list.html"
